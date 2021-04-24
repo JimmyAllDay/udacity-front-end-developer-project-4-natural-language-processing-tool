f0335add42d477f795bade1f39c677a57a48fea0
JimmyAllDay <jameswhmarshall@gmail.com>
2021-04-24 10:34:09 +1000

    still adding get user input

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,11 @@
 import { getUserInput } from './js/getUserInput';
 import { checkForName } from './js/nameChecker';
-import { handleSubmit } from './js/formHandler';
 import './styles/resets.scss';
 import './styles/base.scss';
 import './styles/footer.scss';
 import './styles/form.scss';
 import './styles/header.scss';
 
-console.log(checkForName);
-
 console.log('CHANGE!!');
+
+getUserInput();
--- src/client/js/getUserInput.js
+++ src/client/js/getUserInput.js
@@ -1,4 +1,13 @@
 // ---------------- Variables --------------------------
-let userInput = 'userInput works';
-let inputField = document.getElementById('name');
-let submitButton = document.getElementById('submitButton');
+
+function getUserInput() {
+  let userInput = '';
+  let inputField = document.getElementById('name');
+  let submitButton = document.getElementById('submitButton');
+  submitButton.addEventListener('click', function() {
+    userInput = inputField.value;
+    console.log(userInput);
+  });
+}
+
+export { getUserInput };
